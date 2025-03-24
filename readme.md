0fbe0285a9001ca2e8baa58a5e620b14f7dc13ee
Mohammadreza <hozoorimohammadreza1@gmail.com>
2025-03-24 17:52:13 +0330

    django apps added

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Frontend framework : NextJS + TypeScript
+
 # Backend framework : Django + DRF
+
 the database used in this project is sqlite
+
+## superuser
+
+username: admin
+password: admin1234
--- todoApi/todoApi/settings.py
+++ todoApi/todoApi/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed apps
+    'home',
+    'todo',
+    # installed packages
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
